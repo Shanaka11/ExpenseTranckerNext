5d6ed8b055fbbc9026174db0383fa3190e3b7378
Shanaka Abeysinghe <Shanakaabeysinghe@gmail.com>
2023-07-28 07:16:27 +0530

    Removed localhost

--- src/ClientServices/ClientTransactionServices.ts
+++ src/ClientServices/ClientTransactionServices.ts
@@ -1,5 +1,5 @@
 export const insertTransactionService = async (data: any) => {
-	let response = await fetch('http://localhost:3000/api/transaction', {
+	let response = await fetch('api/transaction', {
 		method: 'POST',
 		body: JSON.stringify(data),
 	});
@@ -7,14 +7,14 @@ export const insertTransactionService = async (data: any) => {
 };
 
 export const deleteTransactionService = async (id: string) => {
-	let response = await fetch(`http://localhost:3000/api/transaction/${id}`, {
+	let response = await fetch(`api/transaction/${id}`, {
 		method: 'DELETE',
 	});
 	return response;
 };
 
 export const updateTransactionService = async (data: any, id: string) => {
-	let response = await fetch(`http://localhost:3000/api/transaction/${id}`, {
+	let response = await fetch(`api/transaction/${id}`, {
 		method: 'PUT',
 		body: JSON.stringify(data),
 	});
