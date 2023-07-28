02e0094e47fad4e3f937aa369b4547175566b9e6
Shanaka Abeysinghe <Shanakaabeysinghe@gmail.com>
2023-07-28 10:30:14 +0530

    Fixed height issue on mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
 	],
 	theme: {
 		extend: {
+			height: {
+				screen: ['100vh', '100dvh'],
+			},
 			backgroundImage: {
 				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
 				'gradient-conic':
