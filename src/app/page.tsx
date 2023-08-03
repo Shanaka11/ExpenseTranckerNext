77a2888636b29bdd75ffc258b252ea5940fbbdd9
Shanaka Abeysinghe <Shanakaabeysinghe@gmail.com>
2023-08-03 13:30:49 +0530

    feature/ Features section finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
 import Button from '@/components/Button';
+import BalanceTrendAnalysisIcon from '@/components/Icons/BalanceTrendAnalysisIcon';
+import CategoryIcon from '@/components/Icons/CategoryIcon';
+import CustomizeIcon from '@/components/Icons/CustomizeIcon';
+import ExpenseOverviewIcon from '@/components/Icons/ExpenseOverviewIcon';
+import FinanciallyInformedIcon from '@/components/Icons/FinanciallyInformedIcon';
+import IncomeBreakdownIcon from '@/components/Icons/IncomeBreakdownIcon';
+import InsightsIcon from '@/components/Icons/InsightsIcon';
+import PieChartIcon from '@/components/Icons/PieChartIcon';
+import RealTimeUpdateIcon from '@/components/Icons/RealTimeUpdateIcon';
+import RegisterTransactionIcon from '@/components/Icons/RegisterTransactionIcon';
+import TopTransactionIcon from '@/components/Icons/TopTransactionIcon';
+import VisualizeIcon from '@/components/Icons/VisualizeIcon';
 import Image from 'next/image';
 export default function Home() {
 	return (
-		<>
+		<div className='col-span-2 overflow-x-auto'>
 			{/* Hero */}
-			<div className='relative col-span-2 grid h-96 place-items-center md:h-[35rem]'>
+			<section className='relative col-span-2 grid h-96 place-items-center md:h-[35rem]'>
 				<Image
 					src='https://five12daysgeneral.s3.ap-southeast-1.amazonaws.com/Expense/35cc357a76a5471b977d99e93dcdad97.jpg'
 					alt='hero'
@@ -16,7 +28,7 @@ export default function Home() {
 				/>
 				<div className='z-10 px-10 md:px-8'>
 					<h1
-						className='text-6xl font-bold text-white'
+						className='text-6xl font-semibold text-white'
 						style={{ textShadow: '-7px 4px 5px black' }}
 					>
 						Take Control of Your Finances
@@ -26,10 +38,118 @@ export default function Home() {
 						<Button label='Try for free' />
 					</div>
 				</div>
-			</div>
+			</section>
 			{/* Features */}
-			<div></div>
+			<section className='bg-blue-400 py-5'>
+				<h2 className='col-span-12 pb-5 text-center text-3xl font-semibold text-white'>
+					Features
+				</h2>
+				<div className='grid grid-cols-12 place-items-center gap-5 px-8 md:gap-10 lg:px-44'>
+					<div className='col-span-12 w-full overflow-hidden rounded-lg bg-white drop-shadow md:col-span-6 lg:col-span-4 lg:w-[350px]'>
+						<div className='relative h-72 min-w-[320px] md:min-w-[370px]'>
+							<Image
+								src='https://five12daysgeneral.s3.ap-southeast-1.amazonaws.com/Expense/TransactionsTab.jpeg'
+								alt='Quick Transactions'
+								fill
+								style={{
+									objectFit: 'cover',
+								}}
+								className='rounded-t-lg'
+							/>
+						</div>
+						<h3 className='my-4 text-center text-xl font-semibold'>
+							Quick Transactions
+						</h3>
+						<ul className='flex flex-col gap-4 px-4 pb-5 md:px-10'>
+							<li className='flex place-items-center gap-4'>
+								<RegisterTransactionIcon />
+								<p>Transaction Recording</p>
+							</li>
+							<li className='flex place-items-center gap-4'>
+								<CategoryIcon />
+								<p>Better Organization</p>
+							</li>
+							<li className='flex place-items-center gap-4'>
+								<CustomizeIcon />
+								<p>Customizable Tags</p>
+							</li>
+							<li className='flex place-items-center gap-4'>
+								<RealTimeUpdateIcon />
+								<p>Real-time Updates</p>
+							</li>
+						</ul>
+					</div>
+					<div className='col-span-12 w-full overflow-hidden rounded-lg bg-white drop-shadow md:col-span-6 lg:col-span-4 lg:w-[350px]'>
+						<div className='relative h-72 min-w-[320px] md:min-w-[370px]'>
+							<Image
+								src='https://five12daysgeneral.s3.ap-southeast-1.amazonaws.com/Expense/AddTransactionMobile.jpeg'
+								alt='Quick Transactions'
+								fill
+								style={{
+									objectFit: 'cover',
+								}}
+								className='rounded-t-lg'
+							/>
+						</div>
+						<h3 className='my-4 text-center text-xl font-semibold'>
+							Financial Snapshot
+						</h3>
+						<ul className='flex flex-col gap-4 px-4 pb-5 md:px-10'>
+							<li className='flex place-items-center gap-4'>
+								<IncomeBreakdownIcon />
+								<p>See Your Income Breakdown</p>
+							</li>
+							<li className='flex place-items-center gap-4'>
+								<ExpenseOverviewIcon />
+								<p>Get an Expense Overview</p>
+							</li>
+							<li className='flex place-items-center gap-4'>
+								<TopTransactionIcon />
+								<p>View Top Transactions</p>
+							</li>
+							<li className='flex place-items-center gap-4'>
+								<PieChartIcon />
+								<p>Explore Spending by Tags</p>
+							</li>
+						</ul>
+					</div>
+					<div className='col-span-12 w-full overflow-hidden rounded-lg bg-white drop-shadow lg:col-span-4 lg:w-[350px]'>
+						<div className='relative h-72 w-full'>
+							<Image
+								src='https://five12daysgeneral.s3.ap-southeast-1.amazonaws.com/Expense/DashboardPC.jpg'
+								alt='Quick Transactions'
+								fill
+								style={{
+									objectFit: 'cover',
+								}}
+								className='rounded-t-lg'
+							/>
+						</div>
+						<h3 className='my-4 text-center text-xl font-semibold'>
+							Financial Overview Hub
+						</h3>
+						<ul className='flex flex-col gap-4 px-4 pb-5 md:px-10'>
+							<li className='flex place-items-center gap-4'>
+								<FinanciallyInformedIcon />
+								<p>Stay Financially Informed</p>
+							</li>
+							<li className='flex place-items-center gap-4'>
+								<VisualizeIcon />
+								<p>Visualize Your Spending</p>
+							</li>
+							<li className='flex place-items-center gap-4'>
+								<BalanceTrendAnalysisIcon />
+								<p>Balance Trend Analysis</p>
+							</li>
+							<li className='flex place-items-center gap-4'>
+								<InsightsIcon />
+								<p>Quick Balance Insights</p>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</section>
 			{/* Footer */}
-		</>
+		</div>
 	);
 }
---
+++ src/components/Icons/VisualizeIcon.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const VisualizeIcon = () => {
+	return (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			height='36'
+			viewBox='0 -960 960 960'
+			width='36'
+			className='fill-blue-400'
+		>
+			<path d='M160-160v-440h140v440H160Zm0-500v-140h140v140H160Zm250 500v-320h140v320H410Zm0-380v-140h140v140H410Zm250 380v-200h140v200H660Zm0-260v-140h140v140H660Z' />
+		</svg>
+	);
+};
+
+export default VisualizeIcon;
